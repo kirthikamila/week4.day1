c8df24fc244d476804873d5e7e7521261e6676e4
kirthikamila <rajukirthika31@gmail.com>
2025-05-23 19:56:41 +0530

    Update MissingElemet.java

--- MissingElemet.java
+++ MissingElemet.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class MissingElemet {
 
 	public static void main(String[] args) {
-		// Step 1: Declare and initialize the array
+		//Declare and initialize the array
         Integer[] numbers = {1, 2, 3, 4, 10, 6, 8};
 
-        // Step 2: Convert the array to a list
+        // Convert the array to a list
         List<Integer> numberList = new ArrayList<>(Arrays.asList(numbers));
 
-        // Step 3: Sort the list in ascending order
+        // Sort the list in ascending order
         Collections.sort(numberList);
 
-        // Step 4: Find the missing numbers
+        //Find the missing numbers
         System.out.println("Missing Numbers:");
         for (int i = 0; i < numberList.size() - 1; i++) {
             int current = numberList.get(i);
