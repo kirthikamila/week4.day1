f153703628189864bdcc7f6f012ea2d1122b23a5
kirthikamila <rajukirthika31@gmail.com>
2025-05-23 19:53:15 +0530

    Update FindIntersection.java

--- FindIntersection.java
+++ FindIntersection.java
@@ -5,18 +5,18 @@ import java.util.List;
 public class FindIntersection {
 
 	public static void main(String[] args) {
-		// Step 1: Declare and initialize arrays
+		//Declare and initialize arrays
         Integer[] array1 = {3, 2, 11, 4, 6, 7};
         Integer[] array2 = {1, 2, 8, 4, 9, 7};
 
-        // Step 2: Convert arrays to lists
+        //Convert arrays to lists
         List<Integer> list1 = new ArrayList<>(Arrays.asList(array1));
         List<Integer> list2 = new ArrayList<>(Arrays.asList(array2));
 
-        // Step 3: Find intersection using retainAll()
+        // Find intersection using retainAll()
         list1.retainAll(list2);
 
-        // Step 4: Print the common elements
+        // Print the common elements
         System.out.println("Intersection of both lists: " + list1);
 
 	}
